38c634b536462e7b852b3fdcdfa6e5c3659b4825
hachondeoro <frank.espinoza@ucsp.edu.pe>
2020-07-29 13:03:22 +0930

    Updates

---
+++ precache-manifest.4a9e33d7be8bc951c16a4cecd72dd165.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1817b01a2e7a689bbd5cba3e9c413709",
+    "url": "/confusion/index.html"
+  },
+  {
+    "revision": "3d768654767e6b795f72",
+    "url": "/confusion/static/css/2.fd308fc8.chunk.css"
+  },
+  {
+    "revision": "30dbffbbb39b63aabd58",
+    "url": "/confusion/static/css/main.3039ffa8.chunk.css"
+  },
+  {
+    "revision": "3d768654767e6b795f72",
+    "url": "/confusion/static/js/2.22dbcbe9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/confusion/static/js/2.22dbcbe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30dbffbbb39b63aabd58",
+    "url": "/confusion/static/js/main.d1f470d9.chunk.js"
+  },
+  {
+    "revision": "93e338425105545936e2",
+    "url": "/confusion/static/js/runtime-main.dfde7455.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
